cacd8184f9e9e22d60d6144118c32a50552eaed6
KennyEmbrechts <kenny.embrechts@gmail.com>
2015-06-08 16:09:58 +0200

    Vraag 4/5 begonnen!

--- KennyEmbrechts1eZit14/KennyEmbrechts1eZit14/MainWindow.xaml.cs
+++ KennyEmbrechts1eZit14/KennyEmbrechts1eZit14/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -18,11 +19,51 @@ namespace KennyEmbrechts1eZit14
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
+    /// 
     public partial class MainWindow : Window
     {
+        public ObservableCollection<Personen> Personenlijst { get; set; }
+        public ObservableCollection<Personen> Gezochtelijst { get; set; }
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void LaadJson(object sender, RoutedEventArgs e)
+        {
+            Personenlijst = new ObservableCollection<Personen>(Mensen.readJsonData());
+            LijstPersonen.ItemsSource = Personenlijst;
+        }
+
+        private void JongerDan30Zoeken(object sender, RoutedEventArgs e)
+        {
+            Gezochtelijst = new ObservableCollection<Personen>();
+
+            var Menskens = Personenlijst.Where(t => t.age<30);
+
+            foreach (var i1 in Menskens)
+            {
+                Gezochtelijst.Add(i1);
+            }
+            LijstPersonen.ItemsSource = Gezochtelijst;
+            
+        }
+
+        private void VrouwenLeeftijden(object sender, RoutedEventArgs e)
+        {
+            var Menskens = Personenlijst.Where(t => t.age > 0).Sum(g => g.age);
+            MessageBox.Show(Convert.ToString(Menskens));
+        }
+
+        private void SorteerOpNaamEnToon3(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void VerhoogLeeftijdMet1(object sender, RoutedEventArgs e)
+        {
+            Personen temp = this.DataContext as Personen;
+            temp.age++;
         }
     }
 }
---
+++ KennyEmbrechts1eZit14/KennyEmbrechts1eZit14/Personen.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using System.IO;
+
+namespace KennyEmbrechts1eZit14
+{
+    public class BaseClass : INotifyPropertyChanged
+    {
+        public void Notify(string p)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(p));
+            }
+        }
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+    
+    public class Rootobject
+    {
+        public Personen[] Personen { get; set; }
+    }
+
+    public class Personen
+    {
+        public string _id { get; set; }
+        public int index { get; set; }
+        public bool isActive { get; set; }
+        public string balance { get; set; }
+        public string picture { get; set; }
+        public int age { get; set; }
+        public string eyeColor { get; set; }
+        public string name { get; set; }
+        public string gender { get; set; }
+        public string geslacht
+        {
+            get { if (gender == "female") return "female.png"; else return "male.png"; }
+        }
+
+        public string company { get; set; }
+        public string email { get; set; }
+        public string phone { get; set; }
+        public string address { get; set; }
+        public string about { get; set; }
+        public string registered { get; set; }
+        public float latitude { get; set; }
+        public float longitude { get; set; }
+        public string[] tags { get; set; }
+        public Friend[] friends { get; set; }
+        public string greeting { get; set; }
+        public string favoriteFruit { get; set; }
+    }
+
+    public class Friend
+    {
+    public int id { get; set; }
+    public string name { get; set; }
+    }
+
+    class Mensen : BaseClass
+    {
+        private static List<Personen> AllePersonen = new List<Personen>();
+        public static List<Personen> readJsonData()
+        {
+            using (StreamReader sr = new StreamReader("personen.json"))
+            {
+                string jsondata = sr.ReadToEnd();
+                Rootobject data = JsonConvert.DeserializeObject<Rootobject>(jsondata);
+                int getal = 0;
+                foreach (var zwembad in data.Personen)
+                {
+                    if (getal != 0)
+                    {
+                        AllePersonen.Add(zwembad);
+                    }
+                    getal++;
+                }
+            }
+
+
+            return AllePersonen;
+        }
+    }
+}
